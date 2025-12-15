39f65ceb10a89e09d88019beee5e855376008042
Ahmed hazem <ahmed.hazem708@gmail.com>
2025-12-15 05:41:38 +0200

    added a new model

--- src/admin/pages/ModelsSeatPageAD.jsx
+++ src/admin/pages/ModelsSeatPageAD.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import MediumSeatMap from '../components/modelsSeat/mediumSeatMap/MediumSeatMap'
+import SmallSeatMap from '../components/modelsSeat/smallSeatMap/SmallSeatMap'
 function ModelsPageAD() {
   return (
-    <div className="flex flex-col lg:flex-row gap-6">
+    <div className="flex flex-col gap-6">
       <div className="flex-1 flex flex-col gap-6">
-
+        <h2 className="text-xl font-bold text-gray-700">Medium Model</h2>
         <MediumSeatMap />
       </div>
 
       <div className="flex-1 flex flex-col gap-6">
+        <h2 className="text-xl font-bold text-gray-700">Small Model (Angled)</h2>
+        <SmallSeatMap />
       </div>
     </div>
   )
