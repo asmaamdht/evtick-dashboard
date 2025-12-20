6a5d143f04a4a79c16603f3fe6b046f0f969b579
marihan <marihantaha355@gmail.com>
2025-12-20 14:40:38 +0200

    modified:   package-lock.json 	modified:   src/admin/components/events/SweetAlert.jsx 	modified:   src/auth/login/Login.jsx 	modified:   src/auth/password/ForgotPassword.jsx

--- src/auth/password/ForgotPassword.jsx
+++ src/auth/password/ForgotPassword.jsx
@@ -3,22 +3,46 @@ import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../../firebase/firebase.config";
 import { Link } from "react-router-dom";
 import AuthLayout from "../components/AuthLayout";
+import { db } from "../../firebase/firebase.config"; 
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleReset = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
+   const normalizedEmail = email.trim().toLowerCase();
 
     try {
-      await sendPasswordResetEmail(auth, email, {
+      //check if email exists in Firestore
+      const q = query(
+        collection(db, "users"),
+        where("email", "==", normalizedEmail)
+      );
+      const snap = await getDocs(q);
+
+      if (snap.empty) {
+        setMessage("Wrong Email.");
+        return;
+      }
+
+      // If exists in Firestore, send reset email via Firebase Auth
+      await sendPasswordResetEmail(auth, normalizedEmail, {
         url: "http://localhost:5173/reset-password",
         handleCodeInApp: true,
       });
+
       setMessage("Password reset link sent! Check your email.");
     } catch (error) {
-      setMessage(error.message);
+      const code = error?.code || "";
+      if (code === "auth/invalid-email") setMessage("Invalid email address.");
+      else setMessage(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -59,10 +83,19 @@ export default function ForgotPassword() {
         {/* BUTTON */}
         <button
           type="submit"
-          className="w-full py-3 bg-[#0f9386] text-white font-semibold rounded-lg shadow-md 
-                     hover:opacity-90 transition"
-        >
-          Send Reset Link
+         disabled={loading}
+          className={`w-full py-3 text-white font-semibold rounded-lg shadow-md transition mt-4
+        ${loading ? "bg-[#0f9386]/70 cursor-not-allowed" : "bg-[#0f9386] hover:opacity-90"}
+      `}
+      >
+        {loading ? (
+          <div className="flex items-center justify-center gap-2">
+            <span className="w-5 h-5 border-2 border-white/60 border-t-white rounded-full animate-spin"></span>
+            Sending Link...
+          </div>
+        ) : (
+          "Send Reset Link"
+        )}
         </button>
       </form>
 
