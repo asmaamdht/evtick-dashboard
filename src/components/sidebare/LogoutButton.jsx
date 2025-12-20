80ace3af9f4733eb6a20899cc97e70587aa9ac91
marihan <marihantaha355@gmail.com>
2025-12-20 18:27:41 +0200

    modified:   src/components/sidebare/LogoutButton.jsx

--- src/components/sidebare/LogoutButton.jsx
+++ src/components/sidebare/LogoutButton.jsx
@@ -4,15 +4,31 @@ import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebase.config";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function LogoutButton() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const logout = async () => {
-    await signOut(auth);
-    dispatch(logoutUser());
-    navigate("/");
+   const logout = async () => {
+    
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You will be logged out from your account.",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#0F9386",
+      cancelButtonColor: "gray",
+      confirmButtonText: "Yes, log out",
+      cancelButtonText: "Cancel",
+    });
+
+    if (result.isConfirmed) {
+      
+      await signOut(auth);
+      dispatch(logoutUser());
+      navigate("/"); 
+    }
   };
 
   return (
