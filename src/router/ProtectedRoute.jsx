ce069c74bce7a26021aba7c04a1349c55698bf88
Asmaa Medhat <asmaamedhat507@gmail.com>
2026-07-30 13:24:05 +0300

    handel loading

--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -1,13 +1,16 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Loader2 } from "lucide-react";
+
 
 export default function ProtectedRoute({ children, allowedRoles = [] }) {
   const { currentUser, role, loading } = useSelector((state) => state.auth);
 
   if (loading) {
     return (
+
       <div className="min-h-screen flex items-center justify-center">
-        <div className="text-xl">Loading...</div>
+        <Loader2 className="w-16 h-16 animate-spin text-primary" strokeWidth={1} />
       </div>
     );
   }
@@ -17,8 +20,8 @@ export default function ProtectedRoute({ children, allowedRoles = [] }) {
 
   // check role
   if (allowedRoles.length > 0 && !allowedRoles.includes(role)) {
-    
-  
+
+
     if (role === "admin") return <Navigate to="/admin" replace />;
     if (role === "organizer") return <Navigate to="/dashboard" replace />;
 
